db485caab1610f581ef54210be12ee6cc444b09c
sakina17 <110530068+sakina17@users.noreply.github.com>
2022-11-18 02:55:58 -0800

    Update add.php

--- add.php
+++ add.php
@@ -1,14 +1,14 @@
 <?php
-
-   include "connect.php";
-       if(isset($_POST['submit']))
-        {
-           $user=$_POST['email'];
-           $pass=$POST['pass'];
-           $sql="INSERT into 'login'('userid','password')values('$user','$pass')";
-           mysqli_query($con,$sql);
-  
-        }
+    if(isset($_POST['submit']))
+    {
+        include "connect.php";
+        $a=$_POST['username'];
+        $b=$_POST['email'];
+        $c=$_POST['contact'];
+        $d=$_POST['city'];
+        $sql="INSERT into `login`(`id`,`username`,`email`,`contact`,`city`) values(NULL,'$a','$b','$c','$d')";
+        mysqli_query($con,$sql);
+    }
 ?>
 <html>
     <head>
